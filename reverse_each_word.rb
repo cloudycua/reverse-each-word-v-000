11a1ff01a61b254c8e95a0bb001be7fa8dd918c6
Christine Cua <cloudycua@gmail.com>
2018-02-23 19:33:33 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
   new_sentence = []
-  sentence.split(" ").collect do |word|
-    new_sentence << word.split.reverse.join
+  sentence.split.collect do |word|
+    new_sentence << word.split("").reverse.join
   end
   new_sentence.join(" ")
 end
